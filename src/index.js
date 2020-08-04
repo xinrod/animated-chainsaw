b564f27a059f22fe3d5a504cc1212efbc05e8c00
Roderick Liu <xinrod@live.unc.edu>
2020-08-04 18:11:47 -0400

    fix big images

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './bootstrap.min.css';
 import './font-awesome.css';
 import './animate.css';
+import './style.css'
 
 ReactDOM.render(
     <App />,
